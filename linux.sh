97481fd75872fdb05d106181e5678cb08c57fc99
Michael Lausch <mla@lausch.at>
2016-06-23 17:04:01 +0200

    Use CXX and AR env variable for compiler if set
    If CXX is set use it, otherwise fall back to g++.
    Same for AR.
    Create a static library instead of a standalone object file.

--- linux.sh
+++ linux.sh
@@ -2,9 +2,8 @@
 
 ./clean.sh
 
-g++ -c guid.cpp -o guid.o -Wall -std=c++11 -DGUID_LIBUUID
-g++ -c test.cpp -o test.o -Wall -std=c++11
-g++ -c testmain.cpp -o testmain.o -Wall
-g++ test.o guid.o testmain.o -o test -luuid
-chmod +x test
-./test
+CXX=${CXX:=g++}
+AR=${AR:=ar}
+
+${CXX} -c guid.cpp -o guid.o -Wall -std=c++11 -DGUID_LIBUUID
+${AR} rv libcrossguid.a guid.o
